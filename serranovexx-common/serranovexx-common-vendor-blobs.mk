ee17ffe87da32f37d3cccf7f71c5e12dba13dfbe
danile71 <danile71@gmail.com>
2016-10-08 03:18:13 +0300

    Fixes

--- serranovexx-common/serranovexx-common-vendor-blobs.mk
+++ serranovexx-common/serranovexx-common-vendor-blobs.mk
@@ -94,16 +94,16 @@ PRODUCT_COPY_FILES += \
 # GPS
 PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/bin/irsc_util:system/bin/irsc_util \
-	$(LOCAL_PATH)/bin/loc_launcher:system/bin/loc_launcher \
 	$(LOCAL_PATH)/lib/libloc_api_v02.so:system/lib/libloc_api_v02.so \
-	$(LOCAL_PATH)/lib/libloc_eng.so:system/lib/libloc_eng.so \
-	$(LOCAL_PATH)/lib/libloc_core.so:system/lib/libloc_core.so \
 	$(LOCAL_PATH)/vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
-	$(LOCAL_PATH)/lib/hw/gps.msm8916.so:system/lib/hw/gps.msm8916.so \
-	$(LOCAL_PATH)/lib/libgps.utils.so:system/lib/libgps.utils.so \
 	$(LOCAL_PATH)/vendor/lib/libgeofence.so:system/vendor/lib/libgeofence.so \
 	$(LOCAL_PATH)/vendor/lib/libizat_core.so:system/vendor/lib/libizat_core.so \
-	$(LOCAL_PATH)/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so
+	$(LOCAL_PATH)/vendor/lib/liblbs_core.so:system/vendor/lib/liblbs_core.so \
+	$(LOCAL_PATH)/vendor/lib/liblocationservice.so:system/vendor/lib/liblocationservice.so \
+	$(LOCAL_PATH)/lib/libgps.utils.so:system/lib/libgps.utils.so \
+        $(LOCAL_PATH)/lib/hw/gps.msm8916.so:system/lib/hw/gps.msm8916.so \
+	$(LOCAL_PATH)/lib/libloc_eng.so:system/lib/libloc_eng.so \
+	$(LOCAL_PATH)/lib/libloc_core.so:system/lib/libloc_core.so
 	
 # Graphics
 PRODUCT_COPY_FILES += \
